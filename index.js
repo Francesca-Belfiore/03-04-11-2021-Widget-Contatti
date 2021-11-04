218bf26e992dd455770b13c76b802f5cbb29cc89
Francesca Belfiore <belfiorefrancesca94@gmail.com>
2021-11-04 12:08:07 +0100

    ricerca funzionante e stile più carino

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /* JSdoc, si scrive come il commento multilinea /**/ 
 /* ma con un asterisco in più all'apertura */
 
-/**per evitare la ripetizione di querySelector dopo*/
+/**per evitare la ripetizione di querySelector 3 volte dopo nella funzione*/
 const q = (selector) => document.querySelector(selector); 
 
-const render = (container, items) => {
-    const elements = items.map((element) =>
+const render = (container, items) => { //(container) funziona
+    const elements = items.map((element) => //data.map funziona
         //element.name + ", " + element.phone + ", " + element.email;
         `<li>
             <fieldset>
@@ -13,7 +13,7 @@ const render = (container, items) => {
                 <p><b>Phone:</b> <a href="tel:${element.phone}">${element.phone}</a></p>
                 <p><b>Email:</b> <a href="mailto:${element.email}">${element.email}</a></p>
             </fieldset>
-        </li>` //template string, stringa racchiusa da `` (alt+96)
+        </li>` //template string, con `` alt+96
     );
 
     const content = elements.join("");
@@ -27,4 +27,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const list = q("ul");
 
     render(list, data);
+
+    // VERSIONE ORIGINAL CON SUBMIT:
+
+    // form.addEventListener("submit", (event) => {
+    //    event.preventDefault();
+    //    console.log("ok");
+    //    const value = input.value.toLowerCase();
+
+    //    const results = data.filter((element) => {
+    //         return element.name.toLowerCase().search(value) > -1
+    //     }); //le funzioni con ritorno si possono concatenare
+
+    //     console.log(results);
+
+    //     render(list,results);
+    // });
+
+
+    //RICERCA MENTRE SI DIGITA:
+
+    input.addEventListener("keyup", (event) => {
+        const value = input.value.toLowerCase().trim();
+ 
+        const results = data.filter((element) => 
+            element.name.toLowerCase().search(value) > -1 ||
+            element.email.toLowerCase().search(value) > -1
+        ); //le funzioni con ritorno si possono concatenare
+ 
+        render(list,results);
+    });
 });
